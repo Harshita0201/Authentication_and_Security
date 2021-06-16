a3f00f0f8cb06e2aa2080084042a745f44f7a896
Harshita0201 <62836828+Harshita0201@users.noreply.github.com>
2021-06-16 20:20:32 +0530

    Level 4 Hashing and salting

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose =require("mongoose");
 // const encrypt = require("mongoose-encryption");
-const md5=require("md5");
+// const md5=require("md5");
+const bcrypt= require("bcrypt");
+const saltRounds=10;
 
 const app=express();
 
@@ -46,20 +48,26 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
 
-  //create a new doc inside the users collection in our userDB
-  const newUser= new User({
-    email: req.body.username,
-    password: md5(req.body.password) //to turn this password to a irreversible hash
-  });
+  //level 4 security
+   bcrypt.hash(req.body.password, saltRounds, function(err, hash){ //takes password as plain text, no of salt rounds and a function that generates hash and stores it in db
+     //create a new doc inside the users collection in our userDB
+     const newUser= new User({
+       email: req.body.username,
+      // password: md5(req.body.password) //to turn this password to a irreversible hash level 3 hashing passwords
+       password:hash
+     });
 
-  //saving the credentials of user or the new doc inside our userDB
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets"); //we only render the secrets page if the user gets successfully registered to our website.
-    }
-  });
+     //saving the credentials of user or the new doc inside our userDB
+     newUser.save(function(err){
+       if(err){
+         console.log(err);
+       }else{
+         res.render("secrets"); //we only render the secrets page if the user gets successfully registered to our website.
+       }
+     });
+
+   });
+
 
 });
 
@@ -68,20 +76,31 @@ app.post("/login",function(req,res){
 
   //get the posted req of email and pass on login page
   const username= req.body.username;
-  const password= md5(req.body.password); //hash the enterd password for later comparison
+  //const password= md5(req.body.password); //hash the enterd password for later comparison
+  const password=req.body.password;
+
+
 
   //using the findOne method of mongoose we find the provided username in our db and store it in array foundUser
   User.findOne({email : username}, function(err, foundUser){
     if(err){
       console.log(err);
     }else{ //if there are no errors
-      if(foundUser){ //if the array of foundUser is true or contains something
-        if(foundUser.password === password){ //we tap into the password of corresponding username and match it with password entered through login page
-          res.render("secrets"); //if and ony if the password matches we render the secrets page
 
+      if(foundUser){ //if the array of foundUser is true or contains something
+      //   if(foundUser.password === password){ //we tap into the password of corresponding username and match it with password entered through login page
+      //     res.render("secrets"); //if and ony if the password matches we render the secrets page
+      //
+      //   }
+
+      bcrypt.compare(password, foundUser.password, function(req,result){ //level 4 security : check the password entered against the hash generated in register page in our db
+        if(result === true){
+            res.render("secrets");
         }
-      }
+      });
     }
+  }
+
   });
 
 });
